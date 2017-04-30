bc9d386da017da8fc4a8569355ac734894d66a97
Bryan Burkhardt <bmburkhardt@alaska.edu>
2017-04-29 19:11:21 -0800

    Cleaned up code a bit

--- src/musicPlayer.cpp
+++ src/musicPlayer.cpp
@@ -63,95 +63,10 @@ void MusicPlayer::draw() {
 	return;
 }
 
-/****** Functionality Function Definitions ******/
-
-void MusicPlayer::callPlayPause() {
-	if (music.getStatus() != music.Playing) {
-		music.play();
-		std::cout << "Playing song: " << songList_[songListIndex_] << std::endl;
-	}
-	else {
-		music.pause();
-	}
-}
-void MusicPlayer::callNextSong() {
-	if(songList_.size() != 0) {
-		music.stop();
-		//if you're at the end, just go to the begining(if press next)
-		if(songListIndex_ == songList_.size()-1) {
-			songListIndex_ = 0;
-		}
-		else {
-			songListIndex_ += 1;
-		}
-		music.openFromFile(songList_[songListIndex_]);
-		music.play();
-		std::cout << "Next song: " << songList_[songListIndex_] << " vecIndex: " << songListIndex_ << std::endl;
-	}
-}
-void MusicPlayer::callPrevSong() {
-	if(songList_.size() != 0) {
-		music.stop();
-
-		//if you're at the begining, just go to the end(if press prev)
-		if(songListIndex_ == 0) {
-			songListIndex_ = songList_.size()-1;
-		}
-		else {
-			songListIndex_ -= 1;
-		}
-		music.openFromFile(songList_[songListIndex_]);
-		music.play();
-		std::cout << "Previous song: " << songList_[songListIndex_] << " vecIndex: " << songListIndex_ << std::endl;
-	}
-}
-void MusicPlayer::callIncreaseVolume() {
-	//first unmutes if muted
-	if(isMuted_ == true)
-	{
-		isMuted_=  false;
-		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
-	}
-	//if we change the " +1" for music.getVolume, make sure to reduce the "<= 99" in the if statement (100 - number) -CS
-	if(music.getVolume() <= 99){
-		music.setVolume(music.getVolume() + 1);
-		std::cout << "The volume is " << music.getVolume() << std::endl;
-	}
-}
-void MusicPlayer::callDecreaseVolume() {
-	//first unmutes if muted
-	if(isMuted_ == true)
-	{
-		isMuted_=  false;
-		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
-	}
-	//if we change the " -1" for music.getVolume, make sure to change the ">= 1" in the if statement to the same value -CS
-	if(music.getVolume() >= 1){
-		music.setVolume(music.getVolume() - 1);
-		std::cout << "The volume is " << music.getVolume() << std::endl;
-	}
-}
-void MusicPlayer::callMuteUnmute() {
-	//if its not muted than set the volume to 0
-	if (isMuted_ == false) {
-		isMuted_ = true;
-		volSave_ = music.getVolume();
-		music.setVolume(0);
-		std::cout << "Muted player." << std::endl;
-	}
-	else {
-		isMuted_=  false;
-		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
-		std::cout << "Unmuted player." << std::endl;
-	}
-}
-
 void MusicPlayer::update() {
 	
 	mouseOverHighlight();
 	displayPlaylist();
-	updateCurrentTimer();
-	updateTotalTimer();
 	displayTimer();
 
 	return;
@@ -327,6 +242,90 @@ MusicPlayer::MusicPlayer() {
 	std::cout << "MusicPlayer initialized" << std::endl;
 }
 
+/****** Functionality Function Definitions ******/
+
+void MusicPlayer::callPlayPause() {
+	if (music.getStatus() != music.Playing) {
+		music.play();
+		std::cout << "Playing song: " << songList_[songListIndex_] << std::endl;
+	}
+	else {
+		music.pause();
+	}
+}
+void MusicPlayer::callNextSong() {
+	if(songList_.size() != 0) {
+		music.stop();
+		//if you're at the end, just go to the begining(if press next)
+		if(songListIndex_ == songList_.size()-1) {
+			songListIndex_ = 0;
+		}
+		else {
+			songListIndex_ += 1;
+		}
+		music.openFromFile(songList_[songListIndex_]);
+		music.play();
+		std::cout << "Next song: " << songList_[songListIndex_] << " vecIndex: " << songListIndex_ << std::endl;
+	}
+}
+void MusicPlayer::callPrevSong() {
+	if(songList_.size() != 0) {
+		music.stop();
+
+		//if you're at the begining, just go to the end(if press prev)
+		if(songListIndex_ == 0) {
+			songListIndex_ = songList_.size()-1;
+		}
+		else {
+			songListIndex_ -= 1;
+		}
+		music.openFromFile(songList_[songListIndex_]);
+		music.play();
+		std::cout << "Previous song: " << songList_[songListIndex_] << " vecIndex: " << songListIndex_ << std::endl;
+	}
+}
+void MusicPlayer::callIncreaseVolume() {
+	//first unmutes if muted
+	if(isMuted_ == true)
+	{
+		isMuted_=  false;
+		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
+	}
+	//if we change the " +1" for music.getVolume, make sure to reduce the "<= 99" in the if statement (100 - number) -CS
+	if(music.getVolume() <= 99){
+		music.setVolume(music.getVolume() + 1);
+		std::cout << "The volume is " << music.getVolume() << std::endl;
+	}
+}
+void MusicPlayer::callDecreaseVolume() {
+	//first unmutes if muted
+	if(isMuted_ == true)
+	{
+		isMuted_=  false;
+		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
+	}
+	//if we change the " -1" for music.getVolume, make sure to change the ">= 1" in the if statement to the same value -CS
+	if(music.getVolume() >= 1){
+		music.setVolume(music.getVolume() - 1);
+		std::cout << "The volume is " << music.getVolume() << std::endl;
+	}
+}
+void MusicPlayer::callMuteUnmute() {
+	//if its not muted than set the volume to 0
+	if (isMuted_ == false) {
+		isMuted_ = true;
+		volSave_ = music.getVolume();
+		music.setVolume(0);
+		std::cout << "Muted player." << std::endl;
+	}
+	else {
+		isMuted_=  false;
+		music.setVolume(volSave_); // unmute the music by restoring the volume to previous value
+		std::cout << "Unmuted player." << std::endl;
+	}
+}
+
+
 /****** Update Functions ******/
 
 void MusicPlayer::mouseOverHighlight() {
@@ -474,6 +473,8 @@ void MusicPlayer::updateTotalTimer() {
 }
 
 void MusicPlayer::displayTimer() {
+	updateCurrentTimer();
+	updateTotalTimer();
 	displayTimerText.setString(current + total);
 }
 int MusicPlayer::convertToMinutes(int seconds) {
