e83dff6b5cb892987bf02cd2af75ee7eb67f0048
Cameron Showalter <cjshowalter@alaska.edu>
2017-04-30 20:18:02 -0800

    Finished refactoring. fixed a bug

--- include/directoryManager.hpp
+++ include/directoryManager.hpp
@@ -16,58 +16,76 @@
 #include <vector>			// vector
 #include <iostream>			// cout endl
 #include <fstream>			// ifstream
+#include <algorithm> 		// find replace remove_if
+#include <locale>        	// tolower
+#include <sys/stat.h>		// used in explorer()
 
-//note: printVec will be deleted once testing is done
-void printVec(std::vector<std::string> & songList);
+//dirent.h used for navigtion
+//if using VisualS, include one, else include the other
+#ifdef _MSC_VER
+	#include "../include/dirent.h"
+#else
+#	include <dirent.h> 
+#endif	
+
 
 class DirectoryManager
 {
 public:
+//Everything is in the same order here and in the .cpp file_.
+
 	DirectoryManager();
-
-	void handleFile();
-
-	std::string getOsName();
-
-	void configFileFound();
-
-	bool handleFileOverride(std::string currLine);
-
+	void handlefile();
+	void defaultSettings();
+	void printWarning();
 	void formatPathForCorrectOS(std::string & path);
-
+	std::string getOsName();
+	void configfileFound();
+	bool isOSother();
 	void explorer(char *dir_name);
+	void removeWhitespaceOutOfQuotes(std::string & currLine);
+	std::string checkPathFormatForOS(const std::string &currLine);
+	bool handlefileOverride(std::string currLine);
+	bool isMusicfile(std::string file_Name);
+
+
+
 
 	std::vector<std::string> getPlaylist();
 
-	bool isMusicFile(std::string fileName);
 
 private:
 	//Overrides can be accessed/changed inside DirectoryConfig.txt
+	std::string configPath_;
+	std::string defaultAudioPath_;
 
-	int operatingSystemOverride;
+	int operatingSystemOverride_;
 	//0 -> program will automatically detect the OS running the program
 	//1 -> program will format directories for "linux/MacOS"
 	//2 -> program will format directories for "Windows"
 	//3 -> program has no idea what path system to use, so "Other"
 	//		Note: if "Other", program will not find any songs. Added for testing purposes
 
-	int uniqueSongOverride;
+	int uniqueSongOverride_;
 	//true - > If songs with same name are found in different directories, includes both in playlist
 	//false -> If songs with same name are found in different directories, only adds the first one to playlist
 
-	std::string yourOS;
+	std::string currOperatingSystem_;
 	//returns your operating system. IS affected by oppereatingSystemOveride
 	//possible returns: "Windows" "linux/MacOS" "Other"
 
+	bool file_Found_;
 
-	std::vector<std::string> songList;
+	bool warningHit_;
+
+	std::vector<std::string> returnedPlaylist_;
 	//it's a list of songs...
 
-	std::vector<std::string> uniqueSongs;
-	//private because no reason for others to use this. it's not equal to songList if
-	//uniqueSongOverride is ever set to true
+	std::vector<std::string> uniqueSongs_;
+	//private because no reason for others to use this. it's not equal to returnedPlaylist_ if
+	//uniqueSongOverride_ is ever set to true
 
-	std::ifstream file;
+	std::ifstream file_;
 	//used in multiple methods. Added to private so i don't have to pass it around.
 
 };
